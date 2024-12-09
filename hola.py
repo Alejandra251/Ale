e16c460ac0d55567ecdf103ee2496642a3eaf032
Alejandra251 <rasalazar7@espe.edu.ec>
2024-12-08 23:33:10 -0500

    control de lectura 2

--- hola.py
+++ hola.py
@@ -1,1 +1,15 @@
-print("Hola, mundo")
+class Cliente {
+    public String nombre;
+    public String apellido;
+    public int telefono;
+    
+    public Cliente(string nombre,string apellido, int telefono) {
+        this.nonmbre =nombre;
+        this.apellido= apellido;
+        this,telefono= telefono;
+    }
+
+    public void registrar cliente() {
+        system.out.println("Cliente registrado:" + nombre + "" + apellido);
+    }
+}
